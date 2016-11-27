9a6396b3a1ae6aebca4dc75a949f9b75a9c9c365
orangefreeze <orangefreezepuz@gmail.com>
2016-11-27 17:22:15 +0000

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = (Math.random() < 0.75 ? 3 : (Math.random() < 0.75 ? 6 : !) );
+    var value = (Math.random() < 0.75 ? 3 : (Math.random() < 0.999 ? 6 : 192) );
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
